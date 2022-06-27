892e53565898e3e88e02a6a439da8f08977f5249
Nico Nussmüller <nico@nussmueller.dev>
2022-06-27 22:07:46 +0200

    fix: updated build command

--- Deployment/deploy.ps1
+++ Deployment/deploy.ps1
@@ -5,7 +5,7 @@ $imageName = "randomword"
 $serverConfig = Get-Content serverConfig.json | ConvertFrom-Json
 
 Set-Location $projectPath
-docker buildx build --platform linux/arm64 --push -t $serverConfig.DockerUsername/$imageName .
+docker buildx build --platform linux/arm64 --push -t $($serverConfig.DockerUsername + "/" + $imageName) .
 
 plink -ssh $serverConfig.Url -P $serverConfig.Port -l $serverConfig.Username -pw $serverConfig.Password -batch "cd $($serverConfig.ServerDirectory); docker compose pull; docker compose down; docker compose up -d"
 
